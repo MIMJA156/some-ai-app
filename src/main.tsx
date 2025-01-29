da1200783bb9f3924754cace7292bf535b882583
mimja <75694661+MIMJA156@users.noreply.github.com>
2025-01-28 23:43:02 -0700

    removed top-level await

--- src/main.tsx
+++ src/main.tsx
@@ -4,27 +4,29 @@ import App from "./App";
 
 import ollama from 'ollama/browser'
 
-let models: string[] = []
-let showApp = false;
-try {
-	let fullModels = await ollama.list();
-	models = fullModels.models.map(m => m.name).reverse();
-	showApp = true;
-} catch (e) { }
+(async () => {
+	let models: string[] = []
+	let showApp = false;
+	try {
+		let fullModels = await ollama.list();
+		models = fullModels.models.map(m => m.name).reverse();
+		showApp = true;
+	} catch (e) { }
 
-function reload() {
-	location.reload();
-}
+	function reload() {
+		location.reload();
+	}
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		{showApp ?
-			<App models={models} /> :
-			<div className="flex flex-col items-center justify-center w-screen h-screen text-2xl">
-				<span>Ollama is not running. Start it and click reload.</span>
-				<button className="underline text-sky-400 hover:cursor-pointer text-3xl" onClick={reload}>
-					reload
-				</button>
-			</div>}
-	</React.StrictMode>,
-);
+	ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+		<React.StrictMode>
+			{showApp ?
+				<App models={models} /> :
+				<div className="flex flex-col items-center justify-center w-screen h-screen text-2xl">
+					<span>Ollama is not running. Start it and click reload.</span>
+					<button className="underline text-sky-400 hover:cursor-pointer text-3xl" onClick={reload}>
+						reload
+					</button>
+				</div>}
+		</React.StrictMode>,
+	);
+})();
